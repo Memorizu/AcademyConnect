6434a293017ddb6321606ff62ce1220c89a78a62
Memorizu <kaidohmary@gmail.com>
2023-09-06 22:40:28 +0300

    add .env.sample

--- app_config/urls.py
+++ app_config/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('academy.urls', namespace='course')),
-    path('users/', include('users.urls', namespace='users'))
+    path('', include('academy.urls', namespace='academy')),
+    path('user/', include('users.urls', namespace='user'))
 ]
