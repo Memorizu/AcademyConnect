82c5ae9f89a36942aa0dd788e423b2fe5e62f556
Memorizu <kaidohmary@gmail.com>
2023-09-06 22:35:42 +0300

    lesson 24.1

--- users/serializer.py
+++ users/serializer.py
@@ -4,5 +4,7 @@ from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    model = User
-    fields = ('username', 'first_name', 'last_name', 'email',)
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
+
--- users/urls.py
+++ users/urls.py
@@ -9,5 +9,5 @@ app_name = UsersConfig.name
 urlpatterns = [
     path('', UserListAPIView.as_view(), name='user_list'),
     path('create', UserCreateAPIView.as_view(), name='user_create'),
-    path('', UserUpdateAPIView.as_view(), name='user_update'),
+    path('update/<int:pk>', UserUpdateAPIView.as_view(), name='user_update'),
 ]
