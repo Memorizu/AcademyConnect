403c701bcb250fe2eb3647477132b91e4b852516
Memorizu <kaidohmary@gmail.com>
2023-09-18 22:30:54 +0300

    add subscription and pagination

--- academy/serializer/course.py
+++ academy/serializer/course.py
@@ -2,15 +2,18 @@ from rest_framework import serializers
 
 from academy.models.course import Course
 from academy.serializer.lesson import LessonViewSerializer
+from academy.serializer.subscription import SubscriptionSerializer, SubscriptionListSerializer
 
 
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
+    subscription = SubscriptionListSerializer(read_only=True, many=True, source='course_subscriptions')
     lessons = LessonViewSerializer(read_only=True, many=True)
+
 
     def get_lessons_count(self, obj):
         return obj.lessons.count()
 
     class Meta:
         model = Course
-        fields = ('id', 'title', 'description', 'image', 'lessons_count', 'lessons')
+        fields = ('id', 'title', 'description', 'image', 'lessons_count', 'lessons', 'subscription')
--- academy/views.py
+++ academy/views.py
@@ -2,17 +2,21 @@ from rest_framework import viewsets, generics, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from academy.models import Subscription
 from academy.models.lesson import Lesson
 from academy.models.course import Course
+from academy.paginators import AcademyPaginator
 from academy.permissions import IsAdmin, IsOwner
 from academy.serializer.course import CourseSerializer
 from academy.serializer.lesson import LessonCreateSerializer, LessonViewSerializer
+from academy.serializer.subscription import SubscriptionSerializer, SubscriptionUpdateSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
     permission_classes = [IsAuthenticated, IsAdmin | IsOwner]
+    pagination_class = AcademyPaginator
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -23,8 +27,13 @@ class CourseViewSet(viewsets.ModelViewSet):
         else:
             queryset = Course.objects.filter(user=self.request.user)
 
-        serializer = self.serializer_class(queryset, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
 
 class LessonCreateAPIView(generics.CreateAPIView):
@@ -40,6 +49,7 @@ class LessonListAPIView(generics.ListAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonViewSerializer
     permission_classes = [IsAuthenticated]
+    pagination_class = AcademyPaginator
 
     def get_queryset(self):
         if self.request.user.has_perm('can_view_lessons') or self.request.user.is_superuser:
@@ -63,3 +73,15 @@ class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonViewSerializer
     permission_classes = [IsAuthenticated, IsOwner]
+
+
+class SubscriptionCreateAPIView(generics.CreateAPIView):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class SubscriptionUpdateAPIView(generics.UpdateAPIView):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionUpdateSerializer
+    permission_classes = [IsAuthenticated]
